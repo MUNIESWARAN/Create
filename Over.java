ed6eb18e472e783cca329b1dd744ae6868f14cf8
JeniJoy <mailtojenitak@gmail.com>
2016-06-04 05:18:13 +0000

    Add files via upload

---
+++ Over.java
@@ -0,0 +1,36 @@
+import java.io.*;
+public class Over{
+public static void main(String as[]){
+
+int val1=30;
+int val2=35;
+int result=addValues(val1,val2);
+System.out.println("Addition of two numbers " +result);
+
+String s1="13";
+String s2="12";
+int result1=addValues(s1,s2);
+System.out.println("Float of Values " + result1);
+
+}
+
+private static int addValues(int i1,int i2)
+{
+return i1 + i2;
+}
+private static int addValues(String f1,String f2)
+{
+int value1=Integer.parseInt(f1);
+int value2=Integer.parseInt(f2);
+return value1+value2;
+
+}
+
+
+
+
+
+
+
+
+}
